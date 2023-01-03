b178a5233b3acadb2a91d78b03f5df7586c92fb9
stephen-c-perry <stephen.christian.perry@gmail.com>
2023-01-03 13:53:06 -0600

    updated prep and split functions

--- prepare.py
+++ prepare.py
@@ -1,65 +1,45 @@
 import env
 import acquire
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
+def prep_telco(df):
+    df['total_charges'] = (df.total_charges + '0').astype('float')
+    df = df.drop(columns = ['internet_service_type_id', 'contract_type_id', 'payment_type_id'])
+    cat_cols = df.drop(columns = 'customer_id').select_dtypes(include=object).columns.to_list()
+    cat_col_bin = []
+    for i in range(len(cat_cols)):
+        if len(df[cat_cols[i]].value_counts()) == 2:
+            cat_col_bin.append(cat_cols[i])
+    bin_dummies = pd.get_dummies(df[cat_col_bin],drop_first=True)
+    not_bin = []
+    for i in range(len(cat_cols)):
+        if len(df[cat_cols[i]].value_counts()) > 2:
+            not_bin.append(cat_cols[i])
+    not_bin = pd.get_dummies(df[not_bin],drop_first=True)
+    df.drop(['gender', 'partner', 'dependents', 'phone_service','multiple_lines', 'online_security', 'online_backup','device_protection', 'tech_support','streaming_tv', 'streaming_movies','paperless_billing','churn', 'contract_type', 'internet_service_type', 'payment_type'], axis =  1, inplace=True)
 
+    df = pd.concat([df, bin_dummies], axis=1)
+    df = pd.concat([df, not_bin], axis=1)
+    return df
 
-
-def prep_telco(telco_df):
-
-    telco_df = telco_df.drop(columns = ['payment_type_id', 'internet_service_type_id', 'contract_type_id'])
-    telco_df.total_charges = (telco_df.total_charges + '0').astype('float')
-
-    categorical_cols = []
-    binary_categories = []
-    multi_categories = []
-    numerical_cols = []
-
-    for col in telco_df:
-        if telco_df[col].dtype == 'object':
-            categorical_cols.append(col)
-        if telco_df[col].value_counts().size > 2:
-            multi_categories.append(col)
-        else:
-            binary_categories.append(col)
-    else:
-        numerical_cols.append(col)
-
-    telco_df.gender = telco_df.gender.replace('Male',1)
-    telco_df.gender = telco_df.gender.replace('Female',0)
-    telco_df.dependents = telco_df.dependents.replace('Yes', 1).replace('No', 0)
-    telco_df.phone_service = telco_df.phone_service.replace('Yes', 1).replace('No', 0)
-    telco_df.paperless_billing = telco_df.paperless_billing.replace('Yes', 1).replace('No', 0)
-    telco_df.partner = telco_df.partner.replace('Yes', 1).replace('No',0)
-
-    return telco_df
-
-# prepped_telco = prep_telco(acquire.get_telco_data())
 
 
 # Split Data (train, validate, test)
 '''
-split_data is a function that takes in a dataframe and a specified target column, splits that data first into train and 
-test dataframes, then splits the train set further into train and validate.  This results in 3 dataframes to use for modeling.
+split_data is a function that takes in a dataframe splits that data first into train_validate and 
+test dataframes, then splits the train_validate set further into train and validate.  This results in 3 dataframes to use for modeling.
 '''
-def split_data(df, target=''):
-    train, test = train_test_split(df, 
-                               train_size = 0.8,
-                               random_state=1349,
-                              stratify=df[target])
-    train, val = train_test_split(train,
-                             train_size = 0.7,
-                             random_state=1349,
-                             stratify=train[target])
-    return train, val, test
 
-    # Need train_test_split function^
-
-
-#spit function
-
-def train_test_split(df, target):
-    train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
-    train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train[target])
+def split_data(df):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on churn_Yes.
+    return train, validate, test DataFrames.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.churn_Yes)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123, 
+                                       stratify=train_validate.churn_Yes)
     return train, validate, test
